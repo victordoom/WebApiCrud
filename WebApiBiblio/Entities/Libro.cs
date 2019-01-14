c778441c0ba34d18cdf72f7888a55d9886b03f34
Victor Medina <victor.doom503@gmail.com>
2019-01-14 08:52:49 -0800

    Entidades

---
+++ WebApiBiblio/Entities/Libro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Libro
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int GeneroID { get; set; }
+    }
+}
