0055ed8bba51146d41fbace142ccd2b2db6e8047
Victor Medina <victor.doom503@gmail.com>
2019-01-14 09:00:33 -0800

    SLC

---
+++ WebApiBiblio/SLC/IService.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SLC
+{
+    public interface IService
+    {
+        Libro CreateLibro(Libro newLibro);
+        // Libro RetrieveLibroByID(int ID);
+        // bool UpdateLibro(int ID, Libro libroToUpdate);
+        //bool RemoveLibro(int ID);
+        // List<Libro> FilterLibrosByGeneroID(int generoID);
+        List<Libro> RetrieveAllLibro();
+    }
+}
