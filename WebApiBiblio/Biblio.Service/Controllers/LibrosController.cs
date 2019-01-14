3c90dfed229637af96805839000ea1c207ac8be6
Victor Medina <victor.doom503@gmail.com>
2019-01-14 09:47:05 -0800

    Creacion Usuario DAL

---
+++ WebApiBiblio/Biblio.Service/Controllers/LibrosController.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using SLC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Biblio.Service.Controllers
+{
+    public class LibrosController : ApiController, IService
+    {
+        public Libro CreateLibro(Libro newLibro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Libro> RetrieveAllLibro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApiBiblio/DAL/UsuarioDAL.cs
@@ -0,0 +1,39 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        private List<Libro> Libros = new List<Libro>();
+        private readonly string connectionString = "Server=CDS1UNIVO27\\SQLEXPRESS;Database=Libro;Trusted_Connection=True;";
+
+        public UsuarioDAL()
+        {
+
+        }
+
+        public Libro CreateLibro(Libro newLibro)
+        {
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                var query = new SqlCommand("INSERT INTO Libro(Nombre, Descripcion, GeneroID) VALUES (@p0, @p1, @p2)", sqlConnection);
+
+                query.Parameters.AddWithValue("@p0", newLibro.Nombre);
+                query.Parameters.AddWithValue("@p1", newLibro.Descripcion);
+                query.Parameters.AddWithValue("@p2", newLibro.GeneroID);
+
+                query.ExecuteNonQuery();
+
+                return newLibro;
+
+            }
+
+        }
+
+    }
+}
